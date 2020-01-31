36021f33ad7f57d0851b4c9d6df21f53d0b86281
Ocin007 <ocin007-github@gmx.de>
2020-01-31 17:14:25 +0100

    edit batch script

--- add-address.bat
+++ add-address.bat
@@ -1,13 +1,14 @@
 @echo off
 set /p fullname="full username: "
 set /p node=<node-address.txt
-:: set node=http://localhost:8080
 IF NOT EXIST users\ (
     md users
 )
 set user_dir=users\%fullname%
+set user_exist=0
 IF EXIST "%user_dir%\" (
     echo %fullname% already exist
+    set user_exist=1
 ) ELSE (
     md "%user_dir%"
     cd "%user_dir%"
@@ -16,8 +17,13 @@ IF EXIST "%user_dir%\" (
     cd ..\..
 )
 java -jar client\target\client-0.0.1-SNAPSHOT.jar --address --node "%node%" --name "%fullname%" --publickey "%user_dir%\key.pub"
+IF "%user_exist%" equ "1" (
+    pause
+    exit
+)
 set /p address_hash="address hash: "
 IF "%address_hash%" equ "" (
+    pause
     exit
 )
 cd "%user_dir%"
